d03f04ed7d31bfec6b1ccb85174eab7514cc4921
Sylvia <snacgyver@gmail.com>
2021-11-23 15:53:24 -0500

    start

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const cors = require("cors");
+const app = express();
+
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connected to DB...🎀"))
+  .catch(() => console.log("Couldn't connect to DB...🎃"));
+
+// Middlewares
+app.use(cors());
+
+// route
+app.get("/duck", (req, res) => {
+  res.send("Works");
+});
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log("Server is running...🎇");
+});
